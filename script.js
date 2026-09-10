7e7464353b34f66158f37a669c8b3b65acb6a742
Dan <danramosmarques123@gmail.com>
2026-09-10 19:13:04 -0300

    correcao gramatical do h2

---
+++ script.js
@@ -0,0 +1,17 @@
+
+const vitrine = document.querySelector('.vitrine');
+const setaEsquerda = document.querySelector('.seta-esquerda');
+const setaDireita = document.querySelector('.seta-direita');
+
+
+const passo = 270;
+
+
+setaDireita.addEventListener('click', () => {
+    vitrine.scrollBy({ left: passo, behavior: 'smooth' });
+});
+
+setaEsquerda.addEventListener('click', () => {
+
+    vitrine.scrollBy({ left: -passo, behavior: 'smooth' });
+});
